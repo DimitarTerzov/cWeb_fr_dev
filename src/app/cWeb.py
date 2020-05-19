3cd46cca838507750998a7f5396a502fc87f370f
Dimitar Terzov <dimiterat@gmail.com>
2020-05-19 09:21:54 +0300

    Fix command 7.

--- src/app/cWeb.py
+++ src/app/cWeb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-__version__ = "1.31"
+__version__ = "1.32"
 import os
 import sys
 import re
@@ -525,25 +525,26 @@ def command7(filepath):
             elif '</Section' in line:
                 in_section = False
 
-            for match in re.finditer(filler_re, line):
-                tag_exists = True
+            if in_section:
 
-                target = match.group().strip()
-                # Pass filler tag with tilde.
-                # They are reported in command 15.
-                if "~" in target:
-                    continue
+                for match in re.finditer(filler_re, line):
+                    tag_exists = True
 
-                if (
-                    not re.match(ur'^{0}{1}?$'.format(skip_tags, punctuation), target, re.UNICODE)
-                    and in_section
-                ):
-                    found[ln] = [7, 'Invalid filler tag', target.encode('utf')]
-                    continue
+                    target = match.group().strip()
+                    # Pass filler tag with tilde.
+                    # They are reported in command 15.
+                    if "~" in target:
+                        continue
 
-            for match in re.finditer(ur'\s{0}\W'.format(possible_missing_tag), line, re.UNICODE):
-                if ln not in found and in_section:
-                    found[ln] = [7, 'Possible filler tag missing #', match.group().encode('utf')]
+                    if (
+                        not re.match(ur'^{0}{1}?$'.format(skip_tags, punctuation), target, re.UNICODE)
+                    ):
+                        found[ln] = [7, 'Invalid filler tag', target.encode('utf')]
+                        continue
+
+                for match in re.finditer(ur'\s{0}\W'.format(possible_missing_tag), line, re.UNICODE):
+                    if ln not in found:
+                        found[ln] = [7, 'Possible filler tag missing #', match.group().encode('utf')]
 
     if not tag_exists:
         found['warning_message'] = 'No fillers tags were found. Please refer to the project \
--- src/command_7/validator_7.py
+++ src/command_7/validator_7.py
@@ -27,25 +27,26 @@ def command7(filepath):
             elif '</Section' in line:
                 in_section = False
 
-            for match in re.finditer(filler_re, line):
-                tag_exists = True
+            if in_section:
 
-                target = match.group().strip()
-                # Pass filler tag with tilde.
-                # They are reported in command 15.
-                if "~" in target:
-                    continue
+                for match in re.finditer(filler_re, line):
+                    tag_exists = True
 
-                if (
-                    not re.match(ur'^{0}{1}?$'.format(skip_tags, punctuation), target, re.UNICODE)
-                    and in_section
-                ):
-                    found[ln] = [7, 'Invalid filler tag', target.encode('utf')]
-                    continue
+                    target = match.group().strip()
+                    # Pass filler tag with tilde.
+                    # They are reported in command 15.
+                    if "~" in target:
+                        continue
 
-            for match in re.finditer(ur'\s{0}\W'.format(possible_missing_tag), line, re.UNICODE):
-                if ln not in found and in_section:
-                    found[ln] = [7, 'Possible filler tag missing #', match.group().encode('utf')]
+                    if (
+                        not re.match(ur'^{0}{1}?$'.format(skip_tags, punctuation), target, re.UNICODE)
+                    ):
+                        found[ln] = [7, 'Invalid filler tag', target.encode('utf')]
+                        continue
+
+                for match in re.finditer(ur'\s{0}\W'.format(possible_missing_tag), line, re.UNICODE):
+                    if ln not in found:
+                        found[ln] = [7, 'Possible filler tag missing #', match.group().encode('utf')]
 
     if not tag_exists:
         found['warning_message'] = 'No fillers tags were found. Please refer to the project \
@@ -55,7 +56,7 @@ page to learn about the required use of filler tags.'
 
 
 if __name__ == '__main__':
-    found = command7("../files/no_tags.trs")
+    found = command7("../files/BBC_DIRATV_swa_01 (11).trs")
     keys = sorted(found.keys())
     for key in keys:
         print key, found[key], found[key][2]
